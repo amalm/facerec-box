4ea5811812d2582d3ffb2b58a309bbbf33113f02
mhaidacher <martin.haidacher@skidata.com>
2019-10-28 14:35:57 +0100

    adding swagger server code to implementation

---
+++ enrollment/swagger_server/controllers/server_controller.py
@@ -0,0 +1,27 @@
+import connexion
+import six
+
+from swagger_server.models.key_values import KeyValues  # noqa: E501
+from swagger_server import util
+
+
+def get_statistics():  # noqa: E501
+    """returns a key/value map describing relevant statistics
+
+     # noqa: E501
+
+
+    :rtype: KeyValues
+    """
+    return 'do some magic!'
+
+
+def get_version():  # noqa: E501
+    """returns the version of the Face Recognition Box implementing this API
+
+     # noqa: E501
+
+
+    :rtype: str
+    """
+    return 'do some magic!'
---
+++ enrollment/swagger_server/models/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.enrollment import Enrollment
+from swagger_server.models.key_value import KeyValue
+from swagger_server.models.key_values import KeyValues
